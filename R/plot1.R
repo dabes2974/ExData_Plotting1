7a5139f967f86803de822b1f0bf3a0140f2d9aaa
dbescond <bescond@ilo.org>
2015-03-06 10:28:25 +0100

    plot 1 rev4

--- R/plot1.R
+++ R/plot1.R
@@ -1,12 +1,12 @@
 
 
 # set wd
-# setwd("I:/Tmp/R/ExData_Plotting1")
+ setwd("I:/Tmp/R/ExData_Plotting1")
 getwd()
 
 # cal  library
 
-require(dplyr); require(lubridate)
+require(dplyr); require(lubridate); library(ggplot2)
 
 X <- as.tbl(read.table("./data/household_power_consumption.txt", sep=";", stringsAsFactors = FALSE, header  = TRUE, na.strings = "?"))
 ####### filter data from 2007-02-01 and 2007-02-02 with lubridate
@@ -23,28 +23,32 @@ glimpse(X)
 
 
 # create plot 1 hist()
+png( file = "png/plot1.png", width =800, height = 800, units = "px")
 
-with(X, hist(Global_active_power, col ="red", main = "Global Active Power", xlab ="Global Active Power (kilowatts)"))
 
-# save plot 1
 
-png( width = 480, height = 480)
+
+require(ggplot2)
+
+
+
+
+png( file = "png/plot1.png", width =480, height = 480, units = "px")
+with(X,hist(Global_active_power, col ="red", main = "Global Active Power", xlab ="Global Active Power (kilowatts)"))
+
 
 dev.copy(png, file = "png/plot1.png")
 
 
-## Copy my plot to a PNG file
-
 dev.off()
 
 
+dev.set(dev.prev())
 
 
 
+# save plot 1
 
-
-
-
-
+dev.copy(png, file = "png/plot1.png")
 
 
